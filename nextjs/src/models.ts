54dd2f555a62000620b73c9df12f0c98452e453d
phelukas <ppedrolukas19@gmail.com>
2024-10-24 21:39:20 -0300

    paginas nexts sem nginx

---
+++ nextjs/src/models.ts
@@ -0,0 +1,18 @@
+
+export type VideoModel = {
+    id: number;
+    title: string;
+    description: string;
+    thumbnail: string;
+    slug: string;
+    published_at: string;
+    likes: number;
+    views: number;
+    tags: string[];
+    // author: {
+    //     id: number;
+    //     name: string;
+    //     avatar: string;
+    // }
+    video_url: string;
+}
